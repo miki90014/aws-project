bc1c6d60685fba3b48266da9015b8f5a24765fd4
Monika <tinusia09@gmail.com>
2024-11-11 13:13:24 +0100

    EEEEEEEEEEEEEEEEEEEEEEEND

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 set +x
 
 source .bashrc
-git clone --single-branch --branch developing https://github.com/$GITHUB_OWNER/$GITHUB_REPO $WORKSPACE
+git clone https://github.com/$GITHUB_OWNER/$GITHUB_REPO $WORKSPACE
 sed -i "s/ENV AWS_REGION=.*/ENV AWS_REGION=$AWS_REGION/" $WORKSPACE/backend/Dockerfile
 sed -i "s/ENV COGNITO_POOL_ID=.*/ENV COGNITO_POOL_ID=$COGNITO_POOL_ID/" $WORKSPACE/backend/Dockerfile
 sed -i "s/ENV COGNITO_CLIENT_ID=.*/ENV COGNITO_CLIENT_ID=$COGNITO_CLIENT_ID/" $WORKSPACE/backend/Dockerfile
