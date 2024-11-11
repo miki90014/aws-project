c857b314b66f47b3a854f6db4efc1d7fe30b3273
Monika <tinusia09@gmail.com>
2024-11-11 10:16:45 +0100

    Cinfig for rds

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "ec2_build_instance_id" {
 output "ec2_build_public_ip" {
   value = aws_instance.proj_ec2_build.public_ip
 }
+
+output "database_url" {
+  value = aws_db_instance.db.endpoint
+}
