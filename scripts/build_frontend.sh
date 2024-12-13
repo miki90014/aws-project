24149a4c70ed5ac39cdf0ffe5c8f584a2a05e731
Monika <tinusia09@gmail.com>
2024-12-13 15:35:29 +0100

    separete ec2

---
+++ scripts/build_frontend.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+set +x
+
+source .bashrc
+git clone -b seperate-ec2 --single-branch https://github.com/$GITHUB_OWNER/$GITHUB_REPO $WORKSPACE
+sed -i "s/ENV FRONTEND_PORT=.*/ENV FRONTEND_PORT=$FRONTEND_PORT/" $WORKSPACE/frontend/Dockerfile
+sed -i "s/ENV BACKEND_SERVICE_NAME=.*/ENV BACKEND_SERVICE_NAME=$BACKEND_SERVICE_NAME/" $WORKSPACE/frontend/Dockerfile
+sed -i 's|"proxy": "http://backend:5000"|"proxy": "'"${BACKEND_SERVICE}:5000"'"|' $WORKSPACE/frontend/package.json
+
+sudo docker build -t frontend-container $WORKSPACE/frontend/Dockerfile
+sudo docker run --name frontend-container -p ${EXPOSED_FRONTEND_PORT}:${FRONTEND_PORT} frontend-react
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,17 @@
-output "ec2_build_instance_id" {
-  value = aws_instance.proj_ec2_build.id
+output "ec2_backend_instance_id" {
+  value = aws_instance.proj_ec2_backend.id
 }
 
-output "ec2_build_public_ip" {
-  value = aws_instance.proj_ec2_build.public_ip
+output "ec2_backend_public_ip" {
+  value = aws_instance.proj_ec2_backend.public_ip
+}
+
+output "ec2_frontend_instance_id" {
+  value = aws_instance.proj_ec2_frontend.id
+}
+
+output "ec2_frontend_public_ip" {
+  value = aws_instance.proj_ec2_frontend.public_ip
 }
 
 output "database_url" {
